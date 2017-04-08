e0510da0ec6a475fe7553ce18fc92b68e4bc5cfd
NRGb3nder <energybender120@gmail.com>
2017-04-08 15:03:20 +0300

    Add tasks

---
+++ fibonacci.asm
@@ -0,0 +1,40 @@
+.686
+.model flat, stdcall
+.stack 4096
+
+ExitProcess PROTO :DWORD
+DumpRegs PROTO
+
+.data
+	fibonacciArray      BYTE    1, 1, 5 DUP (?)
+
+.code
+
+main PROC
+        xor     eax, eax
+        mov     al, fibonacciArray[0]
+        call    DumpRegs
+        mov     al, fibonacciArray[1]
+        call    DumpRegs
+
+        mov     ecx, 5
+        mov     bl, fibonacciArray
+        mov     dl, fibonacciArray + 1
+        mov     esi, 1
+NewNum:
+        mov     al, bl
+        add     al, dl
+        mov     fibonacciArray[esi + 1], al
+        call    DumpRegs
+        mov     bl, fibonacciArray[esi]
+        inc     esi 
+        mov     dl, fibonacciArray[esi]
+        loop    NewNum
+        ; eax: 1 -> 1 -> 2 -> 3 -> 5 -> 8 -> 13 
+        ; fibonacciArray: 1, 1, 2, 3, 5, 8, 13
+
+	    xor     eax, eax
+ 	    invoke  ExitProcess, 0 
+main ENDP
+
+END main
---
+++ math-expression.asm
@@ -0,0 +1,30 @@
+.686
+.model flat, stdcall
+.stack 4096
+
+ExitProcess PROTO :DWORD
+DumpRegs PROTO
+
+.data
+	val1    SDWORD      8
+    val2    SDWORD      -15
+    val3    SDWORD      20    
+
+.code
+
+main PROC
+        ; -val2 + 7 - val3 + val1
+    
+        xor     eax, eax
+        neg     val2
+        add     eax, val2   ; eax = 0Fh
+        add     eax, 7      ; eax = 16h
+        add     eax, val1   ; eax = 1Eh
+        sub     eax, val3   ; eax = 0Ah
+        call    DumpRegs
+
+	    xor     eax, eax
+ 	    invoke  ExitProcess, 0 
+main ENDP
+
+END main

f15c3155e47c4cc2fdb64fd3966442304787cee2
NRGb3nder <energybender120@gmail.com>
2017-04-11 22:28:32 +0300

    Fix tabs

--- ofcf-flags.asm
+++ ofcf-flags.asm
@@ -6,27 +6,27 @@ ExitProcess PROTO :DWORD
 DumpRegs PROTO
 
 .data
-    firstValue      BYTE    01000000b
-    secondValue     BYTE    10000000b
-    thirdValue      BYTE    11111111b 
+        firstValue      BYTE    01000000b
+        secondValue     BYTE    10000000b
+        thirdValue      BYTE    11111111b 
 
 .code
 
 main PROC
-    xor     eax, eax
-    mov     al, firstValue
-    call    DumpRegs    ; OF = 0, CF = 0
-    add     al, firstValue
-    call    DumpRegs    ; OF = 1, CF = 0
-    
-    xor     eax, eax
-    mov     al, secondValue
-    add     al, 0
-    call    DumpRegs    ; OF = 0, CF = 0
-    add     al, thirdValue
-    call    DumpRegs    ; OF = 1, CF = 1 
+        xor     eax, eax
+        mov     al, firstValue
+        call    DumpRegs    ; OF = 0, CF = 0
+        add     al, firstValue
+        call    DumpRegs    ; OF = 1, CF = 0
 
-    invoke  ExitProcess, 0 
+        xor     eax, eax
+        mov     al, secondValue
+        add     al, 0
+        call    DumpRegs    ; OF = 0, CF = 0
+        add     al, thirdValue
+        call    DumpRegs    ; OF = 1, CF = 1 
+
+        invoke  ExitProcess, 0 
 main ENDP
 
 END main

dfdef3ab0625ce7ae2734f543108a2045473e449
NRGb3nder <energybender120@gmail.com>
2017-04-12 17:41:03 +0300

    Add task

---
+++ edication-marks-extended.asm
@@ -0,0 +1,125 @@
+INCLUDE Irvine32.inc
+
+.const
+        MAX_SCORE_A = 100
+        MAX_SCORE_B = 89
+        MAX_SCORE_C = 79
+        MAX_SCORE_D = 69
+        MAX_SCORE_F = 59
+        SCORE_MAX_LIMIT = 100
+
+.data
+        queryCounter      BYTE    0
+        MarkTable         BYTE    MAX_SCORE_F
+                          DWORD   ProcessF
+        TABLE_ELEM_SIZE = ($ - MarkTable)
+                          BYTE    MAX_SCORE_D
+                          DWORD   ProcessD
+                          BYTE    MAX_SCORE_C
+                          DWORD   ProcessC
+                          BYTE    MAX_SCORE_B
+                          DWORD   ProcessB
+                          BYTE    MAX_SCORE_A
+                          DWORD   ProcessA
+        MARKS_QUANTITY = ($ - MarkTable) / TABLE_ELEM_SIZE
+        msgPrompt         BYTE    "Please enter your score 0-100 to get your mark", 0
+        msgMarkFound      BYTE    "Your mark is: ", 0
+        msgMarkNotFound   BYTE    "Mark with specified score not found.", 0
+        msgContinue       BYTE    "Check another score? Press SPACE to repeat search", 0
+        msgQueries        BYTE    "Queries quantity: ", 0
+        msgError          BYTE    "Invalid value! Score should be in [0-100]", 0
+.code
+
+main PROC
+Search:
+        inc     queryCounter
+        call    ClrScr
+        mov     edx, OFFSET msgPrompt
+        call    WriteString
+        call    CrLf
+        xor     eax, eax
+        call    ReadInt
+        call    CheckInput
+
+        mov     ebx, OFFSET MarkTable
+        mov     ecx, MARKS_QUANTITY
+
+Comparison:
+        cmp     al, [ebx]
+        ja      CheckNextMark
+        mov     edx, OFFSET msgMarkFound
+        call    WriteString
+        call    NEAR PTR [ebx + 1]
+        call    WriteChar
+
+        jmp     RepeatSearch
+
+CheckNextMark:
+        add     ebx, TABLE_ELEM_SIZE
+        loop    Comparison
+        mov     edx, OFFSET msgMarkNotFound
+        call    WriteString
+
+RepeatSearch:
+        call    CrLf
+        mov     edx, OFFSET msgContinue
+        call    WriteString
+        call    CrLf
+        call    ReadChar
+        cmp     al, ' '
+        je      Search
+
+QuitReport:
+        call    CrLf
+        mov     edx, OFFSET msgQueries
+        call    WriteString
+        xor     eax, eax
+        mov     al, queryCounter
+        call    WriteInt
+Quit::
+        call    CrLf
+        call    WaitMsg
+
+        invoke  ExitProcess, 0
+main ENDP
+
+CheckInput PROC USES edx eflags
+        call    IsDigit
+        jnz     Error
+        cmp     eax, SCORE_MAX_LIMIT
+        jb      Pass
+Error:
+        mov     edx, OFFSET msgError
+        call    WriteString
+        jmp     Quit
+
+Pass:
+        ret
+CheckInput ENDP
+
+ProcessA PROC
+        mov     al, 'A'
+        ret
+ProcessA ENDP
+
+ProcessB PROC
+        mov     al, 'B'
+        ret
+ProcessB ENDP
+
+ProcessC PROC
+        mov     al, 'C'
+        ret
+ProcessC ENDP
+
+ProcessD PROC
+        mov     al, 'D'
+        ret
+ProcessD ENDP
+
+ProcessF PROC
+        mov     al, 'F'
+        ret
+ProcessF ENDP
+
+END main
